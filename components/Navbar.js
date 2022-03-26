8d6e91f092eb47396891c5746c070425a6a4acfe
[DeveshPurohit] <purohitdev08@gmail.com>
2022-03-26 13:01:08 +0530

    Tshirt page ready

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,12 +5,12 @@ import { AiOutlineShoppingCart } from 'react-icons/ai';
 
 const Navbar = () => {
   return (
-    <div className='flex flex-col md:flex-row md:items-start items-center justify-start py-3'>
+    <div className='flex flex-col md:flex-row md:items-start items-center justify-start py-3 mb-10 shadow-md'>
       <div className="logo mx-5">
-        <Image src={'/logo.png'} height={30} width={160} alt={'logo'} />
+        <Link href={'/'}><a><Image src={'/logo.png'} height={30} width={160} alt={'logo'} /></a></Link>
       </div>
       <div className="nav">
-        <ul className="flex items-center space-x-5 font-bold ">
+        <ul className="flex items-center text-md space-x-5 font-bold ">
           <Link href={'/tshirt'}><a><li>Tshirts</li></a></Link>
           <Link href={'/hoodies'}><a><li>Hoodies</li></a></Link>
           <Link href={'/stickers'}><a><li>Stickers</li></a></Link>
