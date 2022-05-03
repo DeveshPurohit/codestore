92529b654e63cfb001621c6131c66e96bae38665
[DeveshPurohit] <purohitdev08@gmail.com>
2022-05-03 16:58:48 +0530

    pages other than tshirts connected to database

--- pages/mugs.js
+++ pages/mugs.js
@@ -1,185 +1,88 @@
-import React from 'react'
-import Link from 'next/link'
+import React from "react";
+import Link from "next/link";
+import Product from "../models/Product";
+import mongoose from "mongoose";
 
-const Mugs = () => {
+const Mugs = ({ products }) => {
   return (
     <div>
       <section className="text-gray-600 body-font">
         <div className="container px-5 py-10 mx-auto">
-          <div className="flex flex-wrap -m-4">
-            <Link href={"/product/enjoy-the-code"}>
-              <div className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-lg m-5 cursor-pointer">
-                <a className="block relative rounded overflow-hidden">
-                  <img
-                    alt="ecommerce"
-                    className="m-auto h-[36vh] block"
-                    src="https://m.media-amazon.com/images/I/51kjnqqv2YL._AC_UL320_.jpg"
-                  />
-                </a>
-                <div className="mt-4 text-center">
-                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                    Tshirt
-                  </h3>
-                  <h2 className="text-gray-900 title-font text-lg font-medium">
-                    The Catalyzer
-                  </h2>
-                  <p className="mt-1">₹16.00</p>
-                  <p className="className">S, M, L, XL, XXL</p>
-                </div>
-              </div>
-            </Link>
-            <Link href={"/product/enjoy-the-code"}>
-              <div className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-lg m-5 cursor-pointer">
-                <a className="block relative rounded overflow-hidden">
-                  <img
-                    alt="ecommerce"
-                    className="m-auto h-[36vh] block"
-                    src="https://m.media-amazon.com/images/I/51kjnqqv2YL._AC_UL320_.jpg"
-                  />
-                </a>
-                <div className="mt-4 text-center">
-                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                    Tshirt
-                  </h3>
-                  <h2 className="text-gray-900 title-font text-lg font-medium">
-                    Shooting Stars
-                  </h2>
-                  <p className="mt-1">₹21.15</p>
-                  <p className="className">S, M, L, XL, XXL</p>
-                </div>
-              </div>
-            </Link>
-            <Link href={"/product/enjoy-the-code"}>
-              <div className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-lg m-5 cursor-pointer">
-                <a className="block relative rounded overflow-hidden">
-                  <img
-                    alt="ecommerce"
-                    className="m-auto h-[36vh] block"
-                    src="https://m.media-amazon.com/images/I/51kjnqqv2YL._AC_UL320_.jpg"
-                  />
-                </a>
-                <div className="mt-4 text-center">
-                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                    Tshirt
-                  </h3>
-                  <h2 className="text-gray-900 title-font text-lg font-medium">
-                    Neptune
-                  </h2>
-                  <p className="mt-1">₹12.00</p>
-                  <p className="className">S, M, L, XL, XXL</p>
-                </div>
-              </div>
-            </Link>
-            <Link href={"/product/enjoy-the-code"}>
-              <div className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-lg m-5 cursor-pointer">
-                <a className="block relative rounded overflow-hidden">
-                  <img
-                    alt="ecommerce"
-                    className="m-auto h-[36vh] block"
-                    src="https://m.media-amazon.com/images/I/51kjnqqv2YL._AC_UL320_.jpg"
-                  />
-                </a>
-                <div className="mt-4 text-center">
-                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                    Tshirt
-                  </h3>
-                  <h2 className="text-gray-900 title-font text-lg font-medium">
-                    The 400 Blows
-                  </h2>
-                  <p className="mt-1">₹18.40</p>
-                  <p className="className">S, M, L, XL, XXL</p>
-                </div>
-              </div>
-            </Link>
-            <Link href={"/product/enjoy-the-code"}>
-              <div className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-lg m-5 cursor-pointer">
-                <a className="block relative rounded overflow-hidden">
-                  <img
-                    alt="ecommerce"
-                    className="m-auto h-[36vh] block"
-                    src="https://m.media-amazon.com/images/I/51kjnqqv2YL._AC_UL320_.jpg"
-                  />
-                </a>
-                <div className="mt-4 text-center">
-                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                    Tshirt
-                  </h3>
-                  <h2 className="text-gray-900 title-font text-lg font-medium">
-                    The Catalyzer
-                  </h2>
-                  <p className="mt-1">₹16.00</p>
-                  <p className="className">S, M, L, XL, XXL</p>
-                </div>
-              </div>
-            </Link>
-            <Link href={"/product/enjoy-the-code"}>
-              <div className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-lg m-5 cursor-pointer">
-                <a className="block relative rounded overflow-hidden">
-                  <img
-                    alt="ecommerce"
-                    className="m-auto h-[36vh] block"
-                    src="https://m.media-amazon.com/images/I/51kjnqqv2YL._AC_UL320_.jpg"
-                  />
-                </a>
-                <div className="mt-4 text-center">
-                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                    Tshirt
-                  </h3>
-                  <h2 className="text-gray-900 title-font text-lg font-medium">
-                    Shooting Stars
-                  </h2>
-                  <p className="mt-1">₹21.15</p>
-                  <p className="className">S, M, L, XL, XXL</p>
-                </div>
-              </div>
-            </Link>
-            <Link href={"/product/enjoy-the-code"}>
-              <div className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-lg m-5 cursor-pointer">
-                <a className="block relative rounded overflow-hidden">
-                  <img
-                    alt="ecommerce"
-                    className="m-auto h-[36vh] block"
-                    src="https://m.media-amazon.com/images/I/51kjnqqv2YL._AC_UL320_.jpg"
-                  />
-                </a>
-                <div className="mt-4 text-center">
-                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                    Tshirt
-                  </h3>
-                  <h2 className="text-gray-900 title-font text-lg font-medium">
-                    Neptune
-                  </h2>
-                  <p className="mt-1">₹12.00</p>
-                  <p className="className">S, M, L, XL, XXL</p>
-                </div>
-              </div>
-            </Link>
-            <Link href={"/product/enjoy-the-code"}>
-              <div className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-lg m-5 cursor-pointer">
-                <a className="block relative rounded overflow-hidden">
-                  <img
-                    alt="ecommerce"
-                    className="m-auto h-[36vh] block"
-                    src="https://m.media-amazon.com/images/I/51kjnqqv2YL._AC_UL320_.jpg"
-                  />
-                </a>
-                <div className="mt-4 text-center">
-                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                    Tshirt
-                  </h3>
-                  <h2 className="text-gray-900 title-font text-lg font-medium">
-                    The 400 Blows
-                  </h2>
-                  <p className="mt-1">₹18.40</p>
-                  <p className="className">S, M, L, XL, XXL</p>
-                </div>
-              </div>
-            </Link>
+          <div className="flex flex-wrap -m-4 justify-center">
+            {Object.keys(products).length === 0 && <p>Sorry all Mugs are out of stock! New stock Comiong Soon....Stay Tuned.</p>}
+            {Object.keys(products).map((item) => {
+              return <Link passHref={true} key={products[item]._id} href={`/product/${products[item].slug}`}>
+                  <div className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-lg m-5 cursor-pointer">
+                    <a className="block relative rounded overflow-hidden">
+                      <img
+                        alt="ecommerce"
+                        className="m-auto h-[36vh] block"
+                        src={products[item].img}
+                      />
+                    </a>
+                    <div className="mt-4 text-center">
+                      <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                        {products[item].category}
+                      </h3>
+                      <h2 className="text-gray-900 title-font text-lg font-medium">
+                        {products[item].title}
+                      </h2>
+                      <p className="mt-1">₹{products[item].price}</p>
+                      <div>
+                        {products[item].size.includes('S') &&  <span className="border border-gray-300 mx-1 px-1">S</span>}
+                        {products[item].size.includes('M') &&  <span className="border border-gray-300 mx-1 px-1">M</span>}
+                        {products[item].size.includes('L') &&  <span className="border border-gray-300 mx-1 px-1">L</span>}
+                        {products[item].size.includes('XL') &&  <span className="border border-gray-300 mx-1 px-1">XL</span>}
+                        {products[item].size.includes('XXL') &&  <span className="border border-gray-300 mx-1 px-1">XXL</span>}
+                      </div>
+                      <div className="mt-1">
+                      {products[item].color.includes('red') && <button className="border-2 border-gray-300 ml-1 bg-red-700 rounded-full w-6 h-6 focus:outline-none"></button>}
+                      {products[item].color.includes('green') && <button className="border-2 border-gray-300 ml-1 bg-green-700 rounded-full w-6 h-6 focus:outline-none"></button>}
+                      {products[item].color.includes('blue') && <button className="border-2 border-gray-300 ml-1 bg-blue-700 rounded-full w-6 h-6 focus:outline-none"></button>}
+                      {products[item].color.includes('purple') && <button className="border-2 border-gray-300 ml-1 bg-purple-700 rounded-full w-6 h-6 focus:outline-none"></button>}
+                      {products[item].color.includes('black') && <button className="border-2 border-gray-300 ml-1 bg-black rounded-full w-6 h-6 focus:outline-none"></button>}
+                      {products[item].color.includes('grey') && <button className="border-2 border-gray-300 ml-1 bg-gray-600 rounded-full w-6 h-6 focus:outline-none"></button>}
+                      {products[item].color.includes('yellow') && <button className="border-2 border-gray-300 ml-1 bg-yellow-700 rounded-full w-6 h-6 focus:outline-none"></button>}
+                      </div>
+                    </div>
+                  </div>
+                </Link>
+            })}
           </div>
         </div>
       </section>
     </div>
-  )
+  );
+};
+
+export async function getServerSideProps(context) {
+  if (!mongoose.connections[0].readyState) {
+    await mongoose.connect(process.env.MONGO_URI);
+  }
+
+  let products = await Product.find({category: 'mugs'});
+  let mug = {}
+    for(let item of products){
+      if(item.title in mug){
+        if(!mug[item.title].color.includes(item.color) && item.availableQty > 0){
+          mug[item.title].color.push(item.color)
+        }  
+        if(!mug[item.title].size.includes(item.size) && item.availableQty > 0){
+          mug[item.title].size.push(item.size)
+        }
+      }
+      else{
+        mug[item.title] = JSON.parse(JSON.stringify(item))
+        if(item.availableQty > 0){
+          mug[item.title].color = [item.color]
+          mug[item.title].size = [item.size]
+        }
+      }
+    }
+
+  return {
+    props: { products: JSON.parse(JSON.stringify(mug)) }, // will be passed to the page component as props
+  };
 }
 
-export default Mugs
+export default Mugs;
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -219,12 +219,12 @@ const Post = ({addToCart, buyNow , product , variants}) => {
               </div>
               <div className="flex">
                 <span className="title-font font-medium text-2xl text-gray-900">
-                ₹499
+                ₹{product.price}
                 </span>
-                <button onClick={() => {buyNow(slug , 1 , 499 , product.title , size , color)}} className="flex ml-10 text-white bg-blue-500 border-0 py-2 px-1 md:px-4 focus:outline-none hover:bg-blue-600 rounded">
+                <button onClick={() => {buyNow(slug , 1 , product.price , product.title , size , color)}} className="flex ml-10 text-white bg-blue-500 border-0 py-2 px-1 md:px-4 focus:outline-none hover:bg-blue-600 rounded">
                   Buy Now
                 </button>
-                <button onClick={() => {addToCart(slug , 1 , 499 , product.title , size , color)}} className="flex ml-4 text-white bg-blue-500 border-0 py-2 px-1 md:px-4 focus:outline-none hover:bg-blue-600 rounded">
+                <button onClick={() => {addToCart(slug , 1 , product.price , product.title , size , color)}} className="flex ml-4 text-white bg-blue-500 border-0 py-2 px-1 md:px-4 focus:outline-none hover:bg-blue-600 rounded">
                   Add To Cart
                 </button>
                 <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
@@ -271,7 +271,7 @@ export async function getServerSideProps(context) {
   }
 
   let product = await Product.findOne({slug : context.query.slug});
-  let variants = await Product.find({title : product.title})
+  let variants = await Product.find({title : product.title, category: product.category})
   let colorSizeSlug = {}
   for(let item of variants){
     if(Object.keys(colorSizeSlug).includes(item.color)){
